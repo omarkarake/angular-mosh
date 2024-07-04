5f3b92fac866f459ce816f1b343f6b3727b9ef25
omar karake <omar.karake@amalitechtraining.org>
2024-07-04 10:10:34 +0200

    creating a service

---
+++ src/app/course/course.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-course',
+  templateUrl: './course.component.html',
+  styleUrl: './course.component.css',
+})
+export class CourseComponent {
+  title = 'angular';
+  courses: any;
+}
---
+++ src/app/course/course.service.ts
@@ -0,0 +1,5 @@
+export class CourseService {
+  getCouse() {
+    return ['course1', 'course2', 'course3'];
+  }
+}
