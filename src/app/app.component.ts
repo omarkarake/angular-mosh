91c568d802f74f0514a2bcb72feb9b1a4a325559
omar karake <omar.karake@amalitechtraining.org>
2024-07-04 11:03:43 +0200

    exercise one

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { AuthorService } from './services/author.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-lecture';
+  authors;
+  authorLength;
+  constructor(author: AuthorService) {
+    this.authors = author.getAuthor();
+    this.authorLength = this.authors.length;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,19 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import {
+  BrowserModule,
+  provideClientHydration,
+} from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CourseComponent } from './course/course.component';
 import { CourseService } from './course/course.service';
+import { AuthorService } from './services/author.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CourseComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideClientHydration(),
-    CourseService
-  ],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, CourseComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [provideClientHydration(), CourseService, AuthorService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
