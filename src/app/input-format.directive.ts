fd4ca4e108819e59f53fa7fb0d2ba4b025ce4829
omar karake <omar.karake@amalitechtraining.org>
2024-07-07 19:42:59 +0200

    creating custom directive part2,  we can use this way if we have more input we want to change it's behavior

--- src/app/input-format.directive.ts
+++ src/app/input-format.directive.ts
@@ -4,7 +4,7 @@ import { Directive, ElementRef, HostListener, Input } from '@angular/core';
   selector: '[appInputFormat]',
 })
 export class InputFormatDirective {
-  @Input('format') format: any;
+  @Input('appInputFormat') format: any;
   constructor(private el: ElementRef) {}
   @HostListener('focus') onFocus() {
     console.log('focus');
