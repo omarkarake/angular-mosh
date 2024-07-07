352da9d34f6ea8e3672128cb95a30b11eb1dad34
omar karake <omar.karake@amalitechtraining.org>
2024-07-07 19:38:13 +0200

    we use format in order to get what kind of format do we want for our input, and we use format when we only have only single input

--- src/app/input-format.directive.ts
+++ src/app/input-format.directive.ts
@@ -1,15 +1,18 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[appInputFormat]',
 })
 export class InputFormatDirective {
+  @Input('format') format: any;
   constructor(private el: ElementRef) {}
   @HostListener('focus') onFocus() {
     console.log('focus');
   }
   @HostListener('blur') onBlur() {
     let value: string = this.el.nativeElement.value;
+    if (this.format == 'lowercase')
+      this.el.nativeElement.value = value.toLocaleLowerCase();
     this.el.nativeElement.value = value.toUpperCase();
   }
 }
