0c088f61dd015576af7dec2c3e264dfc3967a919
omar karake <omar.karake@amalitechtraining.org>
2024-07-12 10:34:26 +0200

    how to explicitly create formGroup and formControl

--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
-  styleUrl: './login-form.component.css'
+  styleUrl: './login-form.component.css',
 })
 export class LoginFormComponent {
-
+  form = new FormGroup({
+    username: new FormControl(),
+    password: new FormControl(),
+  });
 }
