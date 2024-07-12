defad49c01d8d6f4957bd618b45ca8160b79de7f
omar karake <omar.karake@amalitechtraining.org>
2024-07-12 09:10:30 +0200

    working with selections

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -6,6 +6,10 @@ import { Component } from '@angular/core';
   styleUrl: './contact-form.component.css',
 })
 export class ContactFormComponent {
+  contactMethods = [
+    {id: 1, name: "Email"},
+    {id: 2, name: "Phone"},
+  ]
   log(x: any) {
     console.log(x);
   }
