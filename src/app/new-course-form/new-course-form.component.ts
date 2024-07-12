4859c7bbe5bf9992f60b41566d0b7fc98cb7ce39
omar karake <omar.karake@amalitechtraining.org>
2024-07-12 10:18:38 +0200

    angular form submit the values

--- src/app/new-course-form/new-course-form.component.ts
+++ src/app/new-course-form/new-course-form.component.ts
@@ -3,12 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-new-course-form',
   templateUrl: './new-course-form.component.html',
-  styleUrl: './new-course-form.component.css'
+  styleUrl: './new-course-form.component.css',
 })
 export class NewCourseFormComponent {
   categories = [
-    {id: 1, name: "Development"},
-    {id: 2, name: "Art"},
-    {id: 3, name: "Languages"},
-  ]
+    { id: 1, name: 'Development' },
+    { id: 2, name: 'Art' },
+    { id: 3, name: 'Languages' },
+  ];
+  submit(course: object) {
+    console.log(course);
+  }
 }
